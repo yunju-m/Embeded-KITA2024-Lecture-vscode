d3842b0cad96b0f10084b4741c9ce169260e4195
yunju <dbswnfnal@naver.com>
2024-06-17 12:07:12 +0900

    240617 자바스크립트 변수의 종류

---
+++ javascript/variable1.js
@@ -0,0 +1,14 @@
+// 자바스크립트 변수의 종류
+
+// 1. var
+var v;
+console.log(v); // 선언시에 자동으로 undefined로 초기화 됨
+
+// 2. let
+let l;
+console.log(l);
+
+// 3. const
+//const c; // 상수는 반드시 초기화하고 사용해야 함
+const c = 3.14;
+console.log(c);
