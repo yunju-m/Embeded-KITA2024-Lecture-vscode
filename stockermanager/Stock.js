afc74ef1c7b2dcb6d47f90be9e150ce299d9a507
yunju-m <dbswnfnal@naver.com>
2024-06-26 23:20:22 +0900

    240626 매장,재고 관리 프로그램2

--- stockermanager/Stock.js
+++ stockermanager/Stock.js
@@ -1,11 +1,11 @@
 class Stock {
-    constructor(shno, stno, stname, stamt, stindate) {
-        this.shno = shno;       // 매장번호
+    constructor(stno, stname, stamt, stindate, shno) {
         this.stno = stno;       // 재고번호
         this.stname = stname;   // 재고제품명
         this.stamt = stamt;     //재고수량
         this.stindate = stindate;   // 입고일시
         this.strgdate = Date.now(); // 재고등록일시
+        this.shno = shno;       // 매장번호
     }
 
 }
--- stockermanager/stockermanager.js
+++ stockermanager/stockermanager.js
@@ -1,14 +1,20 @@
 $(() => {
     initLocalStorage();
     printShopList();
+    printStockList();
 
     $("#regShopBtn").on('click', () => {
         regidateShop();
+        initInputShop();
     });
 
     $(".delBtn").on('click', (event) => {
-        console.log("삭제클릭");
-        //removeShop(event.target.id);
+        removeShop(event.target.id);
+    });
+
+    $("#regStockBtn").on('click', () => {
+        regidateStock();
+        initInputStock();
     });
 });
 
@@ -30,7 +36,6 @@ const initLocalStorage = () => {
     }
 };
 
-
 // 매장 목록
 const getShopList = () => {
     return JSON.parse(localStorage.getItem("shopList"));
@@ -41,7 +46,13 @@ const getNextShopSeq = () => {
     const nextShopSeq = Number(localStorage.getItem("shopSeq")) + 1;
     localStorage.setItem("shopSeq", nextShopSeq);
     return nextShopSeq;
+}
 
+// 매장 번호 감소
+const getDelShopSeq = () => {
+    const delShopSeq = Number(localStorage.getItem("shopSeq")) - 1;
+    localStorage.setItem("shopSeq", delShopSeq);
+    return delShopSeq;
 }
 
 // 매장 목록 출력
@@ -60,10 +71,16 @@ const printShopList = () => {
         </tr>
     `);
     }
-    // $(".delBtn").on('click', (event) => {
-    //     console.log("삭제클릭");
-    //     //removeShop(event.target.id);
-    // });
+    // empty 수행 후 리스너 삭제 => 재생성
+    $(".delBtn").on('click', (event) => {
+        removeShop(event.target.id);
+    });
+}
+
+// 매장 입력칸 초기화
+const initInputShop = () => {
+    $("#shname").val('');
+    $("#shtotst").val('');
 }
 
 // 매장 등록
@@ -78,19 +95,71 @@ const regidateShop = () => {
 const removeShop = (id) => {
     const shopList = getShopList();
     const leftShopList = shopList.filter((shop) => shop.shno != parseInt(id.split('delBtn')[1]));
-    console.log(leftShopList);
     localStorage.setItem("shopList", JSON.stringify(leftShopList));
     printShopList();
+    getDelShopSeq();
 }
 
 // 재고 목록
 const getStockList = () => {
+    return JSON.parse(localStorage.getItem("stockList"));
+}
 
+// 재고 번호 등록
+const getNextStockSeq = () => {
+    const nextStockSeq = Number(localStorage.getItem("stockSeq")) + 1;
+    localStorage.setItem("stockSeq", nextStockSeq);
+    return nextStockSeq;
+}
+
+// 재고 번호 감소
+const getDelStockSeq = () => {
+    const delStockSeq = Number(localStorage.getItem("stockSeq")) - 1;
+    localStorage.setItem("stockSeq", delStockSeq);
+    return delStockSeq;
 }
 
 // 재고 등록
 const regidateStock = () => {
+    const stockList = getStockList();
+    stockList.push(new Stock(getNextStockSeq(), $("#stname").val(), $("#stamt").val(), $("#stindate").val(), 1));
+    localStorage.setItem("stockList", JSON.stringify(stockList));
+    printStockList();
+}
 
+// 재고 출력
+const printStockList = () => {
+    $("#stocktable").empty();
+    const stockList = getStockList();
+    const stockListLeng = stockList.length;
+    for (let i = 0; i < stockListLeng; i++) {
+        $("#stocktable").append(`
+        <tr>
+            <th scope="row">${stockList[i].stno}</th>
+            <th scope="row">${stockList[i].stname}</th>
+            <th scope="row">${stockList[i].stamt}</th>
+            <th scope="row">${stockList[i].stindate}</th>
+            <th scope="row">${getDateStr(stockList[i].strgdate)}</th>
+            <th scope="row"><button id=regBtn${stockList[i].stno} class="regBtn">수정</button></th>
+            <th scope="row"><button id=delBtn${stockList[i].stno} class="delBtn">삭제</button></th>
+        </tr>
+    `);
+    }
+    // empty 수행 후 리스너 삭제 => 재생성
+    $(".delBtn").on('click', (event) => {
+        removeStock(event.target.id);
+    });
+}
+
+// 메모의 등록 시간 출력형식 지정함수
+function getDateStr(time) {
+    return moment(time).format("YYYY/MM/DD HH:mm");
+}
+
+// 매장 입력칸 초기화
+const initInputStock = () => {
+    $("#stname").val('');
+    $("#stamt").val('');
 }
 
 // 재고 수정
@@ -99,11 +168,16 @@ const editStock = () => {
 }
 
 // 재고 삭제
-const removeStock = () => {
-
+const removeStock = (id) => {
+    const stockList = getStockList();
+    const leftStockList = stockList.filter((stock) => stock.stno != parseInt(id.split('delBtn')[1]));
+    localStorage.setItem("stockList", JSON.stringify(leftStockList));
+    printStockList();
+    getDelStockSeq();
 }
 
 // 재고 수량 변경
 const changeStockQuantity = () => {
 
 }
+
