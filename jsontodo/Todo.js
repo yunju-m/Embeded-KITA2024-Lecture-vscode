c07a13ac690e9175a3f0fff2b692527af0466f08
yunju <dbswnfnal@naver.com>
2024-07-02 16:56:51 +0900

    240702 fix: tdno 삭제

--- jsontodo/Todo.js
+++ jsontodo/Todo.js
@@ -1,6 +1,5 @@
 class Todo {
-    constructor(tdno, tdcontent) {
-        this.tdno = tdno;               // 번호
+    constructor(tdcontent) {
         this.tdcontent = tdcontent;     // 내용
         this.tdregdate = Date.now();    // 등록일시
         this.tdcompleted = false;       // 완료여부
