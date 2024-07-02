3ebadca0fe75a66a3f358017fb8589cf7ae3b083
yunju <dbswnfnal@naver.com>
2024-07-02 16:01:58 +0900

    240702 fix: todo번호 tdno로 변경

--- jsontodo/Todo.js
+++ jsontodo/Todo.js
@@ -1,6 +1,6 @@
 class Todo {
-    constructor(todo, tdcontent) {
-        this.todo = todo;               // 번호
+    constructor(tdno, tdcontent) {
+        this.tdno = tdno;               // 번호
         this.tdcontent = tdcontent;     // 내용
         this.tdregdate = Date.now();    // 등록일시
         this.tdcompleted = false;       // 완료여부
