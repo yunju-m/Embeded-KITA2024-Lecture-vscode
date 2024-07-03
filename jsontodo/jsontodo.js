8979cf08d3dfd791addc1a42a04446874f877c14
yunju <dbswnfnal@naver.com>
2024-07-03 16:54:52 +0900

    240703 fix: todo리스트 완료여부 기능 수정 Promise를 통한 비동기 처리

--- jsontodo/jsontodo.js
+++ jsontodo/jsontodo.js
@@ -1,20 +1,27 @@
-let seq = 1;
 const requestTodo = (method, url, payload) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url, true);
-    if (payload) {
-        xhr.setRequestHeader('content-type', 'application/json');
-    }
-    xhr.send(payload);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            printTodoList(JSON.parse(xhr.responseText));
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url, true);
+        if (payload) {
+            xhr.setRequestHeader('content-type', 'application/json');
         }
-    }
-}
+        xhr.send(payload);
+        xhr.onload = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                resolve(JSON.parse(xhr.response));
+            } else {
+                reject(`${xhr.status} ${xhr.statusText}`);
+            }
+        }
+    });
+};
 
 // 초기 todo 리스트 목록 호출
-requestTodo("GET", "http://localhost:3000/todos");
+const getTodoList = () => {
+    requestTodo("GET", "http://localhost:3000/todos")
+        .then(res => printTodoList(res))
+        .catch(err => console.error(err));
+};
 
 // todo 리스트 등록 이벤트 리스너
 $("#registBtn").on("click", () => {
@@ -24,29 +31,22 @@ $("#registBtn").on("click", () => {
 
 // todo 리스트 목록 출력
 const printTodoList = todoList => {
+    $("#todoList").empty();
+
     todoList.forEach(todo => {
-        let ul = document.createElement('ul');
-        let li = document.createElement('li');
-        li.innerHTML = getDateStr(todo.tdregdate);
-        ul.appendChild(li);
-
-        let ol = document.createElement('ol');
         li = document.createElement('li');
-        li.innerHTML = `<button id=delBtn${todo.id}>X</button>&nbsp;`;
-        li.innerHTML += `<input type ="checkbox" id ="todoCheck${todo.id}" />&nbsp;`;
-        li.innerHTML += `<p>${todo.tdcontent}</p>`;
-        ol.appendChild(li);
+        li.innerHTML += `<div class="li-container"><input id ="todoCheck${todo.id}" class="form-check-input" type ="checkbox" />
+        <label for="todoCheck${todo.id}" class="form-check-label">${todo.tdcontent}</label>
+        <label for="todoCheck${todo.id}">${getDateStr(todo.tdregdate)}</label>
+        <button id=delBtn${todo.id} class="btn btn-danger">X</button></div>`;
 
-        $("#todoList").append(ul);
-        $("#todoList").append(ol);
+        $("#todoList").append(li);
+        $(`label[for=todoCheck${todo.id}]`).css('text-decoration', todo.tdcompleted ? 'line-through' : '');
+        $(`#todoCheck${todo.id}`).prop('checked', todo.tdcompleted);
 
         // todo 체크박스 이벤트 리스너
-        $("#todoCheck" + todo.id).on("click", checkbox => {
-            if (checkbox.target.checked) {
-                $("p").css('text-decoration', 'line-through');
-            } else {
-                $("p").css('text-decoration', '');
-            }
+        $("#todoCheck" + todo.id).on("click", () => {
+            requestTodo("PATCH", `http://localhost:3000/todos/${todo.id}`, JSON.stringify({ tdcompleted: !todo.tdcompleted }));
         });
 
         // todo 리스트 삭제 이벤트 리스너
@@ -54,8 +54,10 @@ const printTodoList = todoList => {
             requestTodo("DELETE", `http://localhost:3000/todos/${todo.id}`);
         });
     });
-}
+};
 
 const getDateStr = date => {
     return moment(date).format("YYYY.MM.DD HH:mm");
-}
+};
+
+getTodoList();
